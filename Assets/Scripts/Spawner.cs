e1c349ffd8f646b5a58c2fdf10d7bd94181fb9c8
Ryan Malani <ryanmalani@MacBook-Pro.fios-router.home>
2021-11-01 19:43:46 -0400

    first commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+  public GameObject[] myObjects;
+  public Transform[] positions; // array of available spawn points (empty gameObjects)
+  public float beat = (60/105);
+  private float timer; // in between spawn of new GameObject
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+      int randomObjectIndex = Random.Range(0,myObjects.Length); // picking a random index from the array of gameObjects
+
+      if(timer>beat)
+      {
+        GameObject randomObject = Instantiate(myObjects[randomObjectIndex], positions[Random.Range(0,positions.Length)]); // instantiating the random object at one of the 3 positions
+        randomObject.transform.localPosition = Vector3.zero;
+        randomObject.transform.Translate(transform.forward);
+        timer -= beat;
+      }
+
+      timer += Time.deltaTime;
+  }
+}
