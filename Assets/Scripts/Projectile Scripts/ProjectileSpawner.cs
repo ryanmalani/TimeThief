b4b9c85b62d2c2268f2592ea8d4dfa57e5cd0f0f
Ryan Malani <54906363+ryanmalani@users.noreply.github.com>
2021-11-04 09:16:52 -0400

    adding projectile scripts

---
+++ Assets/Scripts/Projectile Scripts/ProjectileSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+
+    // Properties
+    public GameObject[] projectiles;
+    public Transform[] positions;
+    private InputDevice leftController;
+    private InputDevice rightController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      List<InputDevice> devices = new List<InputDevice>();
+      InputDevices.GetDevices(devices);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftController.TryGetFeatureValue(CommonUsages.trigger, out float leftTriggerValue);
+        rightController.TryGetFeatureValue(CommonUsages.trigger, out float rightTriggerValue);
+
+        if(leftTriggerValue > .25f)
+        {
+          GameObject leftProjectile = Instantiate(projectiles[0], positions[0]);
+          leftProjectile.transform.localPosition = Vector3.zero;
+          leftProjectile.transform.Translate(transform.forward);
+        }
+        else if(rightTriggerValue > .25f)
+        {
+          GameObject rightProjectile = Instantiate(projectiles[0], positions[1]);
+          rightProjectile.transform.localPosition = Vector3.zero;
+          rightProjectile.transform.Translate(transform.forward);
+        }
+    }
+}
